8cbd969d1adb0413b65c08b89d9298e4c04601c6
james nielson & virginie trubiano <student@epicodus.com>
2015-03-11 16:52:21 -0700

    done

--- find-replace/src/FindReplace.php
+++ find-replace/src/FindReplace.php
@@ -5,13 +5,43 @@
 
         function find_replace($input_string, $find_input, $replace_input) {
 
-            if ($input_string == $find_input) {
-                return $replace_input;
-            } elseif ($input_string != $find_input) {
-                return $input_string; }
-            else {
-                return 'false';
+            $lc_string = strtolower($input_string);
+            $lc_find = strtolower($find_input);
+            $lc_replace = strtolower($replace_input);
+
+            $exploded_string = explode(" ", $lc_string);
+
+            $result_string = array();
+
+            // $split_find = str_split($lc_find);
+
+            foreach ($exploded_string as $word) {
+
+                $replace_word = str_replace($lc_find, $lc_replace, $word);
+                array_push($result_string, $replace_word . " ");
+                // $split_word = str_split($word);
+
+                // if ($split_word == $split_find) {
+                //     if (compare next letter)
+                //     array_push($result_string, $lc_replace . " ");
+                //     // return $replace_input; }
+                //  else {
+                //     array_push($result_string, $word . " ");
+                //     // return $word;
+
             }
+
+            $re_string = implode($result_string);
+            $trim_re_string = trim($re_string);
+            return ucfirst($trim_re_string);
+
+            // if ($input_string == $find_input) {
+            //     return $replace_input;
+            // } elseif ($input_string != $find_input) {
+            //     return $input_string; }
+            // else {
+            //     return 'false';
+            // }
 
 
     }
--- find-replace/tests/FindReplaceTest.php
+++ find-replace/tests/FindReplaceTest.php
@@ -5,7 +5,7 @@
     class FindReplaceTest extends PHPUnit_Framework_TestCase
     {
 
-        // TEST 0: Super-Baby
+/*        // TEST 0: Super-Baby
         function test_0() {
             // ARRANGE
             $test_FindReplace = new FindReplace;
@@ -50,7 +50,65 @@
             $this->assertEquals("a", $result);
         }
 
+*/        // TEST 3: Single Word String
 
+        function test_3() {
+            // ARRANGE
+            $test_FindReplace = new FindReplace;
+            $input_string = "Dog";
+            $find_input = "Dog";
+            $replace_input = "Cat";
+
+            //ACT
+            $result = $test_FindReplace->find_replace($input_string, $find_input, $replace_input);
+
+            //ASSERT
+            $this->assertEquals("Cat", $result);
+        }
+
+        // TEST 4: Multiple Word String
+        function test_4() {
+            // ARRANGE
+            $test_FindReplace = new FindReplace;
+            $input_string = "My dog has fleas";
+            $find_input = "Dog";
+            $replace_input = "Cat";
+
+            //ACT
+            $result = $test_FindReplace->find_replace($input_string, $find_input, $replace_input);
+
+            //ASSERT
+            $this->assertEquals("My cat has fleas", $result);
+        }
+
+        // TEST 5: Multiple Word String Odd
+        function test_5() {
+            // ARRANGE
+            $test_FindReplace = new FindReplace;
+            $input_string = "mY dOg has Fleas";
+            $find_input = "DOg";
+            $replace_input = "CaT";
+
+            //ACT
+            $result = $test_FindReplace->find_replace($input_string, $find_input, $replace_input);
+
+            //ASSERT
+            $this->assertEquals("My cat has fleas", $result);
+        }
+        // TEST 6: Word Within Word
+        function test_6() {
+            // ARRANGE
+            $test_FindReplace = new FindReplace;
+            $input_string = "The cat was in the cathedral";
+            $find_input = "cat";
+            $replace_input = "dog";
+
+            //ACT
+            $result = $test_FindReplace->find_replace($input_string, $find_input, $replace_input);
+
+            //ASSERT
+            $this->assertEquals("The dog was in the doghedral", $result);
+        }
 
     }
 
